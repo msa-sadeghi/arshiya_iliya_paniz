3487a30c22c787fc0524762099522f905d420aeb
msa <msai.sadeghi@gmail.com>
2023-10-22 16:57:01 +0330

    s

--- s2.py
+++ s2.py
@@ -1,2 +1,34 @@
-print("hello every one")
-print("welocome to python class")
+# print("hello every one")
+# print("welcome to python class")
+
+print(1+2)
+print("1+2")
+print("paniz")
+
+x = 12
+print(x)
+
+
+name = "iliya"
+print(name)
+print(type(name))
+print(name[0])
+print(name[1])
+print(name[4])
+print(name[-1])
+
+# TODO
+"""
+ دو متغیر تعریف کنید
+ در اولی
+1
+در دومی 
+2
+را قرار دهید
+مقادیر این دو متغیر را با هم جمع، ضرب تقسیم و تفریق کنید
+و حاصل را نمایش دهید
+
+یک متغیر تعریف کنید اسم خودتان در آن قرار دهید
+هر یک از کاراکترهای اسمتان را پرینت کنید
+
+"""
